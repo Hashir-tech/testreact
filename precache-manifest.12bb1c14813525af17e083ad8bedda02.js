54a2db0b017458aabc751b81032db72c70897a2d
hashir amjad <hashiramjad8@gmail.com>
2020-05-20 15:52:21 +0500

    Updates

---
+++ precache-manifest.12bb1c14813525af17e083ad8bedda02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de2875d553d357303ea1dcce7020a42",
+    "url": "https://hashir-tech.github.io/testreact/index.html"
+  },
+  {
+    "revision": "ba9683d028752457005d",
+    "url": "https://hashir-tech.github.io/testreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ece971c4daebc0d8771a",
+    "url": "https://hashir-tech.github.io/testreact/static/js/2.be6b33bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://hashir-tech.github.io/testreact/static/js/2.be6b33bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9683d028752457005d",
+    "url": "https://hashir-tech.github.io/testreact/static/js/main.6b029b8e.chunk.js"
+  },
+  {
+    "revision": "c7d0fb62d0dc3e2bb0e7",
+    "url": "https://hashir-tech.github.io/testreact/static/js/runtime-main.f58f0b76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://hashir-tech.github.io/testreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
